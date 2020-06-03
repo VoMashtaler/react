03f1c590b530d1658a90f846c964d87a77746a21
VoMashtaler <mashtalerv.p@gmail.com>
2020-06-03 03:47:44 +0300

    61 less1

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import * as axios from 'axios';
 import { connect } from 'react-redux';
-import { setAuthUserData } from '../../redux/auth-reducer-reducer';
+import { setAuthUserData } from '../../redux/auth-reducer';
 
 class HeaderContainer extends React.Component {
     componentDidMount () {
@@ -11,8 +11,8 @@ class HeaderContainer extends React.Component {
         })
         .then(response => {
            if (response.data.resultCode === 0) {
-               let {userId, email, login} = response.data.data;
-               this.props.setAuthUserData (userId, email, login);
+               let {id, email, login} = response.data.data;
+               this.props.setAuthUserData (id, email, login);
            }
         });
         
@@ -22,5 +22,9 @@ class HeaderContainer extends React.Component {
     return <Header {...this.props} />
     }
 };
-const mapStateToProps = (state) => ({})
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth,
+    login: state.auth.login,
+
+})
 export default connect (mapStateToProps, {setAuthUserData})(HeaderContainer);
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -6,6 +6,7 @@ let initialState = {
      userId: null,
      email: null,
      login: null,
+     isAuth: false
      
 };
 
@@ -14,7 +15,8 @@ const authReducer = (state = initialState, action) => {
         case SET_USER_DATA:
             return {
                 ...state,
-                ...action.data
+                ...action.data,
+                isAuth: true
             }        
         default:
             return state;
