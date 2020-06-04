bc74669150eeba654cbcc43c304eef773f55f8f5
VoMashtaler <mashtalerv.p@gmail.com>
2020-06-04 18:48:17 +0300

    66 less1

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -30,11 +30,11 @@ let Users = (props) => {
                     <div>
                         {u.followed
                             ? <button disabled={props.followingInProgress.some(id => id === u.id)} 
-                            onClick={() => {props.unfollow(true, u.id)}}                                
+                            onClick={() => {props.unfollow(u.id)}}                                
                             >Unfollow</button>
 
                             : <button disabled={props.followingInProgress.some(id => id === u.id)} 
-                                    onClick={() => { props.follow(true, u.id)}}                               
+                                    onClick={() => { props.follow(u.id)}}                               
                             >Follow</button>}
 
                     </div>
