7c4df0f2bfbe79b4c374b4354ff7853a2bed767f
VoMashtaler <mashtalerv.p@gmail.com>
2020-06-05 03:16:45 +0300

    69 less

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -2,12 +2,12 @@
 import { updateNewMessageBodyCreator, sendMessageCreator } from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
 import { connect } from "react-redux";
+import { withAuthRedirect } from "../../hoc/withAuthRedirect";
 
 
 let mapStateToProps = (state) => {
     return {
-        dialogsPage: state.dialogsPage,
-        isAuth: state.auth.isAuth
+        dialogsPage: state.dialogsPage
     }
 }
 
@@ -22,7 +22,8 @@ let mapDispatchToProps = (dispatch) => {
         }
     }
 }
+let AuthRedirectComponent = withAuthRedirect(Dialogs);
 
-const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(AuthRedirectComponent);
 
 export default DialogsContainer;
