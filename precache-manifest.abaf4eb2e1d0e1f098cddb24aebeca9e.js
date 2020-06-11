c7e01774b674550c9073e5ca4d74c7325194d60a
VoMashtaler <mashtalerv.p@gmail.com>
2020-06-12 02:32:27 +0300

    Updates

---
+++ precache-manifest.abaf4eb2e1d0e1f098cddb24aebeca9e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e994c268f34f991171bd59a5491b8ca",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "538a2008d92f6bbf7e63",
+    "url": "/react/static/css/3.6198b994.chunk.css"
+  },
+  {
+    "revision": "55faad3e9d5e291a551d",
+    "url": "/react/static/css/main.c2303590.chunk.css"
+  },
+  {
+    "revision": "9b732a290f14bec21d22",
+    "url": "/react/static/js/2.b7022943.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.b7022943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538a2008d92f6bbf7e63",
+    "url": "/react/static/js/3.b4dfc73a.chunk.js"
+  },
+  {
+    "revision": "55faad3e9d5e291a551d",
+    "url": "/react/static/js/main.89811adc.chunk.js"
+  },
+  {
+    "revision": "aeba714677fd15ff5b03",
+    "url": "/react/static/js/runtime-main.8a7d7409.js"
+  },
+  {
+    "revision": "ff622222db24779e2c875ed7fd425e91",
+    "url": "/react/static/media/preloader2.ff622222.svg"
+  },
+  {
+    "revision": "b7a4df4191abc351ab245fab65bff76e",
+    "url": "/react/static/media/user1.b7a4df41.png"
+  }
+]);
